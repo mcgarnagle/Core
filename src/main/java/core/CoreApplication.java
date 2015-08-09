3c1e218d8402da8031358965924706a9d6f85b8f
mcgarnaglesolutions <rick@mcgarnaglesolutions.com.au>
2015-08-09 20:26:05 +1000

    - controller setup and DAO saving to database via JPA

--- src/main/java/core/CoreApplication.java
+++ src/main/java/core/CoreApplication.java
@@ -1,13 +1,29 @@
 package core;
 
+import javax.persistence.EntityManager;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import core.entity.User;
+import core.persistence.PersistenceManager;
 
 @SpringBootApplication
 public class CoreApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(CoreApplication.class, args);
-        System.out.println("hello");
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(CoreApplication.class, args);
+		System.out.println("hello");
+
+//		User user = new User();
+//		user.setUsername("rick");
+//
+//		EntityManager em = PersistenceManager.INSTANCE.getEntityManager();
+//		em.getTransaction().begin();
+//		em.persist(user);
+//		em.getTransaction().commit();
+//		em.close();
+//		PersistenceManager.INSTANCE.close();
+//		System.out.println("done");
+	}
 }
